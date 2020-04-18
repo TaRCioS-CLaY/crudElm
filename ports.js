7174dbdc8090ae50c092269719aed01c189545e2
TaRCioS-CLaY <tarcios.clay@gmail.com>
2020-04-18 02:36:57 -0300

    Delete feito e update incompleto

--- ports.js
+++ ports.js
@@ -5,19 +5,57 @@ let app = Elm.Main.init({
     flags: JSON.stringify(pessoasNoStorage)
 });
 
-app.ports.cadastrar.subscribe((pessoa) =>{
-    pessoasNoStorage = JSON.parse(localStorage.getItem('pessoas')) || [];
+// app.ports.cadastrar.subscribe((pessoa) =>{
+//     pessoasNoStorage = JSON.parse(localStorage.getItem('pessoas')) || [];
 
-    pessoasNoStorage.push(pessoa)
+//     pessoasNoStorage.push(pessoa)
 
-    localStorage.setItem('pessoas', JSON.stringify(pessoasNoStorage));
+//     localStorage.setItem('pessoas', JSON.stringify(pessoasNoStorage));
 
-    Swal.fire(
-        pessoa.nome,
-        'Cadastrado com sucesso',
-        'success'
-    );
+//     Swal.fire(
+//         pessoa.nome,
+//         'Cadastrado com sucesso',
+//         'success'
+//     );
+
+// });
+
+
+// app.ports.editar.subscribe((pessoa) =>{
+//     pessoasNoStorage = JSON.parse(localStorage.getItem('pessoas')) || [];
+
+// })
+
+app.ports.apagar.subscribe((pessoa) =>{
+
+    Swal.fire({
+        title: 'Certeza que deseja excluir?',
+        text: pessoa.nome,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'Cancelar',
+        confirmButtonText: 'Excluir'
+    }).then((result) => {
+
+        if (result.value) {
+
+            app.ports.pessoaApagada.send(pessoa);
+
+            Swal.fire(
+                'Excluído!',
+                'Pessoa excluída com sucesso',
+                'success'
+            );
+        }
+    });
 
 })
 
 
+app.ports.toJs.subscribe((listaPessoas) =>{
+
+    localStorage.setItem('pessoas', JSON.stringify(listaPessoas));
+
+})
--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,12 @@
 port module Main exposing (..)
 
 import Browser exposing (..)
+import Dict exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Json.Decode exposing (..)
+import Json.Encode exposing (..)
 
 
 
@@ -28,7 +30,11 @@ main =
 type alias Model =
     { nome : String
     , idade : Int
-    , listaPessoas : List Pessoa
+    , nomeE : String
+    , idadeE : Int
+    , listaPessoas : Dict Int Pessoa
+    , editandoPessoa : Int
+    , apagandoPessoa : Int
     }
 
 
@@ -40,12 +46,12 @@ type alias Pessoa =
 
 init : String -> ( Model, Cmd Msg )
 init pList =
-    ( Model "" 0 (getPessoas pList), Cmd.none )
+    ( Model "" 0 "" 0 (Dict.fromList (getPessoas pList)) -1 0, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    Sub.none
+    pessoaApagada Apagar
 
 
 
@@ -55,17 +61,30 @@ subscriptions _ =
 type Msg
     = Nome String
     | Idade String
-    | Salvar Model
+    | NomeEditado String
+    | IdadeEditada String
+    | Salvar
+    | Apagar Pessoa
+    | ConfirmApagar Pessoa Int
+    | Editar (Maybe Int)
+    | AceitaEdicao Int Pessoa
+    | Cancelar
 
 
 
+-- | PessoaApagadaJS Pessoa
 -- | Lista String
+-- port cadastrar : Pessoa -> Cmd msg
+-- port editar : Pessoa -> Cmd msg
 
 
-port cadastrar : Pessoa -> Cmd msg
+port apagar : Pessoa -> Cmd msg
 
 
-port pessoasCadastradas : (String -> msg) -> Sub msg
+port pessoaApagada : (Pessoa -> msg) -> Sub msg
+
+
+port toJs : List Pessoa -> Cmd msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -82,13 +101,60 @@ update msg model =
                 Nothing ->
                     ( { model | idade = verificarStringInt idade }, Cmd.none )
 
-        Salvar _ ->
-            ( { model | listaPessoas = Pessoa model.nome model.idade :: model.listaPessoas }, cadastrar (Pessoa model.nome model.idade) )
+        NomeEditado nome ->
+            ( { model | nomeE = nome }, Cmd.none )
+
+        IdadeEditada idade ->
+            case String.toInt idade of
+                Just idadeCerta ->
+                    ( { model | idadeE = idadeCerta }, Cmd.none )
+
+                Nothing ->
+                    ( { model | idadeE = verificarStringInt idade }, Cmd.none )
+
+        Salvar ->
+            let
+                pessoaNovaNaLista =
+                    -- Pessoa model.nome model.idade (codigoPessoa model.listaPessoas) :: model.listaPessoas
+                    Dict.insert (codigoPessoa model.listaPessoas) (Pessoa model.nome model.idade) model.listaPessoas
+            in
+            ( { model | listaPessoas = pessoaNovaNaLista }, toJs (Dict.values pessoaNovaNaLista) )
+
+        Apagar _ ->
+            let
+                pessoaExcluidaDaLista =
+                    -- removePessoaLista pessoa.codigo model.listaPessoas
+                    removePessoaLista model.apagandoPessoa model.listaPessoas
+            in
+            ( { model | listaPessoas = pessoaExcluidaDaLista }, toJs (Dict.values pessoaExcluidaDaLista) )
+
+        ConfirmApagar pessoa codigo ->
+            ( { model | apagandoPessoa = codigo }, apagar pessoa )
+
+        Editar codigoParaEditar ->
+            case codigoParaEditar of
+                Just codigo ->
+                    ( { model | editandoPessoa = codigo }, Cmd.none )
+
+                Nothing ->
+                    ( { model | editandoPessoa = -1 }, Cmd.none )
+
+        AceitaEdicao codigoEdicao pessoa ->
+            let
+                pessoaEditadaNaLista =
+                    Dict.update codigoEdicao (editaPessoa pessoa) model.listaPessoas
+
+                -- editaPessoaLista codigoEdicao pessoa model.listaPessoas
+            in
+            ( { model | listaPessoas = pessoaEditadaNaLista, editandoPessoa = -1 }, toJs (Dict.values pessoaEditadaNaLista) )
+
+        Cancelar ->
+            ( { model | editandoPessoa = -1 }, Cmd.none )
 
 
 
--- Lista pessoas ->
---     ( { model | listaPessoas = getPessoas pessoas }, Cmd.none )
+-- PessoaApagadaJS pessoa ->
+--     ( { model | listaPessoas = removePessoaLista pessoa.nome model.listaPessoas }, Cmd.none )
 -- VIEWS
 
 
@@ -109,7 +175,7 @@ view model =
                         [ input [ class "input is-rounded ", type_ "text", placeholder "Idade", Html.Attributes.value (idadeIntToString model.idade), onInput Idade ] []
                         ]
                     ]
-                , button [ class "button is-success is-rounded", Salvar model |> onClick ] [ text "Cadastrar" ]
+                , button [ class "button is-success is-rounded", Salvar |> onClick ] [ text "Cadastrar" ]
                 ]
             ]
         , div [ class "hero" ]
@@ -120,9 +186,15 @@ view model =
                             [ thead []
                                 [ th [] [ text "Nome" ]
                                 , th [] [ text "Idade" ]
+                                , th [] []
+                                , th [] []
                                 ]
-                            , tbody []
-                                (List.map linhaPessoa model.listaPessoas)
+                            , tbody [] <|
+                                List.map
+                                    (linhaPessoa model.editandoPessoa model)
+                                    (Dict.toList
+                                        model.listaPessoas
+                                    )
                             ]
                         ]
                     ]
@@ -155,23 +227,144 @@ verificarStringInt string =
 pessoaDecoder : Decoder Pessoa
 pessoaDecoder =
     map2 Pessoa
-        (field "nome" string)
-        (field "idade" int)
+        (field "nome" Json.Decode.string)
+        (field "idade" Json.Decode.int)
 
 
-getPessoas : String -> List Pessoa
+getPessoas : String -> List ( Int, Pessoa )
 getPessoas listaPessoas =
     case Json.Decode.decodeString (Json.Decode.list pessoaDecoder) listaPessoas of
-        Ok pessoa ->
-            pessoa
+        Ok lista ->
+            -- List.map (\p -> ( codigoPessoa (Dict.fromList (List.indexedMap Tuple.pair lista)), p )) lista
+            lista
+                |> List.indexedMap Tuple.pair
 
         Err _ ->
             []
 
 
-linhaPessoa : Pessoa -> Html Msg
-linhaPessoa pessoa =
-    tr []
-        [ td [] [ text pessoa.nome ]
-        , td [] [ text (String.fromInt pessoa.idade) ]
-        ]
+
+-- linhaPessoa2 : Int -> Pessoa -> Html Msg
+-- linhaPessoa2 editando pessoa =
+--     tr []
+--         [ td [] [ inputEdicaoNome pessoa editando ]
+--         , td [ style "width" "6rem" ] [ inputEdicaoIdade pessoa editando ]
+--         , td []
+--             [ botaoEditarConfirmar pessoa editando
+--             ]
+--         , td []
+--             [ botaoExcluirCancelar pessoa editando
+--             ]
+--         ]
+
+
+linhaPessoa : Int -> Model -> ( Int, Pessoa ) -> Html Msg
+linhaPessoa editando pModel ( codigo, pessoa ) =
+    if editando == codigo then
+        tr []
+            [ td [] [ input [ class "input is-rounded is-small", type_ "text", placeholder "Nome", Html.Attributes.value pModel.nomeE, onInput NomeEditado ] [] ]
+            , td [ style "width" "6rem" ] [ input [ class "input is-rounded is-small", style "width" "4rem", type_ "text", placeholder "Idade", Html.Attributes.value (String.fromInt pModel.idadeE), onInput IdadeEditada ] [] ]
+            , td []
+                [ button [ class "button is-small is-focused is-rounded is-info", AceitaEdicao codigo (Pessoa pModel.nomeE pModel.idadeE) |> onClick ]
+                    [ text "Salvar" ]
+                ]
+            , td []
+                [ button [ class "button  is-small is-focused is-rounded is-danger", Cancelar |> onClick ]
+                    [ text "Cancelar" ]
+                ]
+            ]
+
+    else
+        tr []
+            [ td [] [ text pessoa.nome ]
+            , td [ style "width" "6rem" ] [ text (String.fromInt pessoa.idade) ]
+            , td []
+                [ button [ class "button is-small is-focused is-rounded is-info", Editar (Just codigo) |> onClick ]
+                    [ i [ class "fas fa-user-edit" ]
+                        []
+                    ]
+                ]
+            , td []
+                [ button [ class "button  is-small is-focused is-rounded is-danger", ConfirmApagar pessoa codigo |> onClick ]
+                    [ i [ class "fas fa-user-times" ]
+                        []
+                    ]
+                ]
+            ]
+
+
+removePessoaLista : Int -> Dict Int Pessoa -> Dict Int Pessoa
+removePessoaLista codigo lista =
+    -- List.filter (\e -> e.codigo /= codigo) lista
+    -- Dict.filter (\cod e -> cod /= codigo) lista
+    Dict.remove codigo lista
+
+
+editaPessoaLista : Int -> Pessoa -> Dict Int Pessoa -> Dict Int Pessoa
+editaPessoaLista codigo edicaoPessoa lista =
+    Dict.update codigo (\p -> p) lista
+
+
+editaPessoa : Pessoa -> Maybe Pessoa -> Maybe Pessoa
+editaPessoa pEditada pessoa =
+    case pessoa of
+        Just p ->
+            Just { p | nome = pEditada.nome, idade = pEditada.idade }
+
+        Nothing ->
+            pessoa
+
+
+
+-- Dict.map codigo (editaPessoa codigo edicaoPessoa) lista
+-- editaPessoa : Int -> Pessoa -> Pessoa -> Pessoa
+-- editaPessoa codigo pessoaAntiga pessoaEditada =
+--     if pessoaAntiga.codigo == codigo then
+--         { pessoaAntiga | nome = pessoaEditada.nome, idade = pessoaEditada.idade }
+--     else
+--         pessoaAntiga
+-- inputEdicaoNome : Pessoa -> Int -> Html Msg
+-- inputEdicaoNome pessoa codigo =
+--     if codigo == pessoa.codigo then
+--         input [ class "input is-rounded is-small", type_ "text", placeholder "Nome", Html.Attributes.value pessoa.nome ] []
+--     else
+--         text pessoa.nome
+-- inputEdicaoIdade : Pessoa -> Int -> Html Msg
+-- inputEdicaoIdade pessoa codigo =
+--     if codigo == pessoa.codigo then
+--         input [ class "input is-rounded is-small", style "width" "4rem", type_ "text", placeholder "Idade", Html.Attributes.value (String.fromInt pessoa.idade) ] []
+--     else
+--         text (String.fromInt pessoa.idade)
+-- botaoEditarConfirmar : Pessoa -> Int -> Html Msg
+-- botaoEditarConfirmar pessoa codigo =
+--     if codigo == pessoa.codigo then
+--         button [ class "button is-small is-focused is-rounded is-info" ]
+--             [ text "Salvar" ]
+--     else
+--         button [ class "button is-small is-focused is-rounded is-info", Editar pessoa.codigo |> onClick ]
+--             [ i [ class "fas fa-user-edit" ]
+--                 []
+--             ]
+-- botaoExcluirCancelar : Pessoa -> Int -> Html Msg
+-- botaoExcluirCancelar pessoa codigo =
+--     if codigo == pessoa.codigo then
+--         button [ class "button  is-small is-focused is-rounded is-danger", Apagar pessoa |> onClick ]
+--             [ text "Cancelar" ]
+--     else
+--         button [ class "button  is-small is-focused is-rounded is-danger", ConfirmApagar pessoa |> onClick ]
+--             [ i [ class "fas fa-user-times" ]
+--                 []
+--             ]
+
+
+codigoPessoa : Dict Int Pessoa -> Int
+codigoPessoa lista =
+    if Dict.isEmpty lista then
+        1
+
+    else if Dict.size lista == 1 then
+        2
+
+    else
+        Dict.keys lista
+            |> List.sum
